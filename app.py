feeb4d3f40f0a55353d58762779b9374072bca7d
Vahida Vadiya <vahidavadiya@gmail.com>
2021-04-10 14:03:29 -0400

    conflict resolved

--- app.py
+++ app.py
@@ -9,11 +9,7 @@ import bcrypt
 import urllib
 
 from datetime import datetime, timedelta
-
-from forms import CustomerSignupForm, CustomerLoginForm, AddProductForm, ChangePasswordForm, OrderForm, UsersForm
-
-
-from forms import CustomerSignupForm, CustomerLoginForm, AddProductForm, AddProductFromAdminForm, ChangePasswordForm, OrderForm, UsersForm
+from forms import CustomerSignupForm, CustomerLoginForm, AddProductForm, AddProductFromAdminForm, ChangePasswordForm, OrderForm, UsersForm, ChangeAddress, ChangePersonalInfo
 from flask_mongoengine import MongoEngine
 from werkzeug.utils import secure_filename
 import mongoengine as me
